5ae20e1729a0ec2a9f34503ed2645dce26b4efac
plmwa <townstgak@gmail.com>
2021-12-20 22:56:27 +0900

    file追加した

--- main.py
+++ main.py
@@ -1,1 +1,57 @@
-]
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
+from awscrt import io, mqtt, auth, http
+from awsiot import mqtt_connection_builder
+import time as t
+import json
+
+# Define ENDPOINT, CLIENT_ID, PATH_TO_CERTIFICATE, PATH_TO_PRIVATE_KEY, PATH_TO_AMAZON_ROOT_CA_1, MESSAGE, TOPIC, and RANGE
+ENDPOINT = "agmxgja9ihm7-ats.iot.ap-northeast-1.amazonaws.com"
+CLIENT_ID = "testDevice"
+PATH_TO_CERTIFICATE = "./secret/certificate.pem.crt"
+PATH_TO_PRIVATE_KEY = "./secret/private.pem.key"
+PATH_TO_AMAZON_ROOT_CA_1 = "./secret/AmazonRootCA1.pem"
+
+TOPIC = ""
+RANGE = 5
+
+#部屋番号きく
+print("合言葉を入力してね")
+TOPIC=input()
+
+
+
+# Spin up resources
+event_loop_group = io.EventLoopGroup(1)
+host_resolver = io.DefaultHostResolver(event_loop_group)
+client_bootstrap = io.ClientBootstrap(event_loop_group, host_resolver)
+mqtt_connection = mqtt_connection_builder.mtls_from_path(
+            endpoint=ENDPOINT,
+            cert_filepath=PATH_TO_CERTIFICATE,
+            pri_key_filepath=PATH_TO_PRIVATE_KEY,
+            client_bootstrap=client_bootstrap,
+            ca_filepath=PATH_TO_AMAZON_ROOT_CA_1,
+            client_id=CLIENT_ID,
+            clean_session=False,
+            keep_alive_secs=6
+            )
+print("Connecting to {} with client ID '{}'...".format(
+        ENDPOINT, CLIENT_ID))
+# Make the connect() call
+connect_future = mqtt_connection.connect()
+# Future.result() waits until a result is available
+connect_future.result()
+print("Connected!")
+# Publish message to server desired number of times.
+print('Begin Publish')
+for i in range (RANGE):
+    data = "plmwa"
+    nioi = "100"
+    message = {"name" : data,"value" : nioi}
+    mqtt_connection.publish(topic=TOPIC, payload=json.dumps(message), qos=mqtt.QoS.AT_LEAST_ONCE)
+    print(json.dumps(message))
+    t.sleep(0.1)
+print('Publish End')
+disconnect_future = mqtt_connection.disconnect()
+disconnect_future.result()
